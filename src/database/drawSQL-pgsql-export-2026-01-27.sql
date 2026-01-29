78493d28fe1940aa86948429480394083b5ca51b
Meaumeau <morgann.derick@eleve.ensai.fr>
2026-01-29 11:15:47 +0100

    ajout table user

--- src/database/drawSQL-pgsql-export-2026-01-27.sql
+++ src/database/drawSQL-pgsql-export-2026-01-27.sql
@@ -19,10 +19,21 @@ ALTER TABLE "ranks" ADD PRIMARY KEY("id");
 CREATE TABLE "players"(
     "id" BIGINT NOT NULL,
     "platform_id" BIGINT NOT NULL,
-    "platform_user_id" VARCHAR(255) NOT NULL, 
+    "platform_user_id" VARCHAR(255) NOT NULL,
     "name" VARCHAR(255) NOT NULL
 );
 ALTER TABLE "players" ADD PRIMARY KEY("id");
+
+CREATE TABLE "users"(
+    "id" BIGINT NOT NULL,
+    "username" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "date_creation" DATE NOT NULL,
+    "ingame_account_id" BIGINT,
+    CONSTRAINT "unique_ingame_link" UNIQUE ("ingame_account_id")
+);
+ALTER TABLE "users" ADD PRIMARY KEY("id");
 
 /* ==========================================
    2. TABLES DE MATCHS & STRUCTURE
@@ -40,8 +51,8 @@ ALTER TABLE "matches" ADD PRIMARY KEY("id");
 
 CREATE TABLE "match_teams"(
     "id" BIGINT NOT NULL,
-    "match_id" UUID NOT NULL,
-    "color" VARCHAR(10) NOT NULL, 
+    "match_id" BIGINT NOT NULL,
+    "color" VARCHAR(10) NOT NULL,
     "score" INTEGER NOT NULL,
     "possession_time" FLOAT(53) NOT NULL,
     "time_in_side" FLOAT(53) NOT NULL
@@ -72,7 +83,7 @@ CREATE TABLE "stats_core"(
     "saves" INTEGER NOT NULL,
     "assists" INTEGER NOT NULL,
     "score" INTEGER NOT NULL,
-    "shooting_percentage" FLOAT(53) NOT NULL, 
+    "shooting_percentage" FLOAT(53) NOT NULL,
     "demo_inflicted" INTEGER NOT NULL,
     "demo_taken" INTEGER NOT NULL
 );
@@ -134,7 +145,7 @@ CREATE TABLE "stats_movement"(
 );
 ALTER TABLE "stats_movement" ADD PRIMARY KEY("participation_id");
 
-CREATE TABLE "stats_positioning"( 
+CREATE TABLE "stats_positioning"(
     "participation_id" BIGINT NOT NULL,
     "average_distance_to_ball" INTEGER NOT NULL,
     "average_distance_to_ball_possession" INTEGER NOT NULL,
@@ -170,33 +181,37 @@ ALTER TABLE "stats_positioning" ADD PRIMARY KEY("participation_id");
    4. RELATIONS ET CLÉS ÉTRANGÈRES
    ========================================== */
 
-/* Lien Joueur -> Plateforme */
-ALTER TABLE "players" 
+/* Link Player -> Plateform */
+ALTER TABLE "players"
 ADD CONSTRAINT "fk_players_platform" FOREIGN KEY("platform_id") REFERENCES "platforms"("id");
 
-/* Lien Match -> Teams */
-ALTER TABLE "match_teams" 
+/* Link User -> Player */
+ALTER TABLE "users"
+ADD CONSTRAINT "fk_users_players" FOREIGN KEY("ingame_account_id") REFERENCES "players"("id");
+
+/* Link Match -> Teams */
+ALTER TABLE "match_teams"
 ADD CONSTRAINT "fk_match_teams_match" FOREIGN KEY("match_id") REFERENCES "matches"("id") ON DELETE CASCADE;
 
-/* Liens Participation -> Team, Joueur, Rang */
-ALTER TABLE "match_participation" 
+/* Links Participation -> Team, Player, Rank */
+ALTER TABLE "match_participation"
 ADD CONSTRAINT "fk_participation_team" FOREIGN KEY("match_team_id") REFERENCES "match_teams"("id") ON DELETE CASCADE;
 
-ALTER TABLE "match_participation" 
+ALTER TABLE "match_participation"
 ADD CONSTRAINT "fk_participation_player" FOREIGN KEY("player_id") REFERENCES "players"("id");
 
-ALTER TABLE "match_participation" 
+ALTER TABLE "match_participation"
 ADD CONSTRAINT "fk_participation_rank" FOREIGN KEY("rank_id") REFERENCES "ranks"("id");
 
 /* Liens Stats (Enfant) -> Participation (Parent) */
-ALTER TABLE "stats_core" 
+ALTER TABLE "stats_core"
 ADD CONSTRAINT "fk_stats_core_participation" FOREIGN KEY("participation_id") REFERENCES "match_participation"("id") ON DELETE CASCADE;
 
-ALTER TABLE "stats_boost" 
+ALTER TABLE "stats_boost"
 ADD CONSTRAINT "fk_stats_boost_participation" FOREIGN KEY("participation_id") REFERENCES "match_participation"("id") ON DELETE CASCADE;
 
-ALTER TABLE "stats_movement" 
+ALTER TABLE "stats_movement"
 ADD CONSTRAINT "fk_stats_movement_participation" FOREIGN KEY("participation_id") REFERENCES "match_participation"("id") ON DELETE CASCADE;
 
-ALTER TABLE "stats_positioning" 
+ALTER TABLE "stats_positioning"
 ADD CONSTRAINT "fk_stats_positioning_participation" FOREIGN KEY("participation_id") REFERENCES "match_participation"("id") ON DELETE CASCADE;
