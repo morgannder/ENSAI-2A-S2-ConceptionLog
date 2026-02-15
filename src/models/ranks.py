765cd7b3e99ae2ddbc41464a8909e27d37dbf962
N-Mehdi <mehdineji14@gmail.com>
2026-02-15 23:19:23 +0100

    ajout de valeurs par défauts nulles pour id, tier et division

--- src/models/ranks.py
+++ src/models/ranks.py
@@ -1,9 +1,9 @@
 class Ranks:
-    def __init__(self, id: int, tier: int, division: int, name: str):
+    def __init__(self, name: str, id: int = 0, tier: int = 0, division: int = 0):
+        self._name = name
         self._id = id
         self._tier = tier
         self._division = division
-        self._name = name
 
     @property
     def id(self):
