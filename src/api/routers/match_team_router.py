1cc7881c7dfa15dfc290cc5f173b0047459635a6
GregLaGreg <gregoireferton6@gmail.com>
2026-03-04 20:24:48 +0100

    router team

--- src/api/routers/match_team_router.py
+++ src/api/routers/match_team_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, status
 
-from src.api.schemas.stats_response import MatchTeamResponse
+from src.dto.match_team_dto import MatchTeamResponseDTO
 from src.service.match_teams_service import MatchTeamService
 
 
@@ -10,8 +10,8 @@ match_team_service = MatchTeamService()
 
 
 @router.get(
-    "/by-match-team/{match_team_id}",
-    response_model=MatchTeamResponse,
+    "/team/{match_team_id}",
+    response_model=MatchTeamResponseDTO,
     summary="Extract match_team information using match_team ID",
     description="Renvoie les informations d'un match_team à partir de son id",
 )
@@ -23,6 +23,35 @@ def get_match_team_by_id(match_team_id: int):
         )
     try:
         match = match_team_service.get_match_team_by_id(match_team_id)
+
+        if match is None:
+            raise HTTPException(status_code=404, detail="Match not found")
+
+        return match
+
+    except HTTPException:
+        raise
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Erreur interne du serveur: {str(e)}",
+        ) from e
+
+
+@router.get(
+    "/match/{match_id}",
+    response_model=list[MatchTeamResponseDTO],
+    summary="Extract 2 match_team using match ID",
+    description="Renvoie les informations des 2 match_team correspondant à un match",
+)
+def get_match_team_by_match_id(match_id: str):
+    if not match_id:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Le paramètre match_id est requis.",
+        )
+    try:
+        match = match_team_service.get_teams_by_match(match_id)
 
         if match is None:
             raise HTTPException(status_code=404, detail="Match not found")
